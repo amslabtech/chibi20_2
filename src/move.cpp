16e96dedb5032a4cac6e144667ad8edb03546582
ibuki1805 <chiffon9109@gmail.com>
2020-02-19 16:24:19 +0900

    go_straight

--- src/move.cpp
+++ src/move.cpp
@@ -1,8 +1,7 @@
 #include <ros/ros.h>
-#include <roomba_500driver_meiji/RoombaCtrl.h>
-#include <nav_msgs/Odometry.h>
-#include <iostream>
-#include <tf/tf.h>
+#include "roomba_500driver_meiji/RoombaCtrl.h"
+#include "nav_msgs/Odometry.h"
+#include "tf/tf.h"
 // using namespace std;
 
 nav_msgs::Odometry odometry;
@@ -10,24 +9,39 @@ nav_msgs::Odometry init_odometry;
 int phase=0;
 double theta=0.0;
 double init_theta=0.0;
-
+int sleep_flag=0;
 
 void odometry_callback(const nav_msgs::Odometry::ConstPtr& odo)
 {
   odometry = *odo;
 }
 
+void sleep_init()
+{
+    if(sleep_flag>=10)
+    {
+        phase=1;
+        init_theta=  tf::getYaw(odometry.pose.pose.orientation);
+        theta=  tf::getYaw(odometry.pose.pose.orientation);
+        init_odometry.pose.pose.position.x=odometry.pose.pose.position.x;
+        init_odometry.pose.pose.position.y=odometry.pose.pose.position.y;
+    }
+    sleep_flag++;
+}
+
+
 roomba_500driver_meiji::RoombaCtrl  go_straight()
 {
     roomba_500driver_meiji::RoombaCtrl control;
-    if(fabs(pow(init_odometry.pose.pose.position.x-odometry.pose.pose.position.x,2)+pow(init_odometry.pose.pose.position.y-odometry.pose.pose.position.y,2))<=1.0)
+    // if(fabs(pow(init_odometry.pose.pose.position.x-odometry.pose.pose.position.x,2)+pow(init_odometry.pose.pose.position.y-odometry.pose.pose.position.y,2))<=1.0)
+    if(fabs(init_odometry.pose.pose.position.x-odometry.pose.pose.position.x)<1.0)
     {
-        control.cntl.linear.x=0.1;
+        control.cntl.linear.x=0.2;
         control.cntl.angular.z=0.0;
     }
     else
     {
-        phase=1;
+        phase=2;
     }
     return control;
 }
@@ -35,14 +49,14 @@ roomba_500driver_meiji::RoombaCtrl  go_straight()
 roomba_500driver_meiji::RoombaCtrl turn_a_round()
 {
     roomba_500driver_meiji::RoombaCtrl control;
-    if((theta-init_theta)>=M_PI*2*0.3)
+    if(fabs(theta-init_theta)<=M_PI*0.5)
     {
         control.cntl.linear.x=0.0;
         control.cntl.angular.z=0.1;
     }
     else
     {
-        phase=2;
+        phase=3;
     }
     return control;
 }
@@ -58,36 +72,42 @@ int   main (int argc, char **argv)
   // {
   //     init_odometry.pose.pose.position.x=odometry.pose.pose.position.x;
   //     init_odometry.pose.pose.position.y=odometry.pose.pose.position.y;
-  //     init_theta=  tf::getYaw(odometry.pose.pose.orientation);
-  //     theta=  tf::getYaw(odometry.pose.pose.orientation);
+      // init_theta=  tf::getYaw(odometry.pose.pose.orientation);
+      // theta=  tf::getYaw(odometry.pose.pose.orientation);
   // }while(odometry.pose.pose.position.x==0);
 
   while(ros::ok())
   {
     roomba_500driver_meiji::RoombaCtrl control;
-    control.mode = roomba_500driver_meiji::RoombaCtrl::DRIVE_DIRECT;
+    control.mode = 11;//roomba_500driver_meiji::RoombaCtrl::DRIVE_DIRECT;
     theta=  tf::getYaw(odometry.pose.pose.orientation);
     switch(phase)
     {
         case(0):
-            control=go_straight();
+            sleep_init();
             break;
         case(1):
+            control=go_straight();
+            break;
+        case(2):
             control=turn_a_round();
             break;
         default:
             break;
     }
-    if(phase==2)
+    if(phase==3)
     {
          control.cntl.linear.x=0.0;
          control.cntl.angular.z=0.0;
     }
+    control.mode = 11;//roomba_500driver_meiji::RoombaCtrl::DRIVE_DIRECT;
     pub_control.publish(control);
     ros::spinOnce();
     loop_rate.sleep();
+    std::cout << "theta:"<<theta << std::endl;
+    std::cout << "phase:"<<phase << std::endl;
     std::cout << "odometry:"<<odometry.pose.pose.position << std::endl;
-    std::cout << "init_odometry:"<<init_odometry.pose.pose.position <<std:: endl;
+    // std::cout << "init_odometry:"<<init_odometry.pose.pose.position <<std:: endl;
   }
 
   return 0;
