e344fcf866b772518377a85239daf992609fe4dc
iharayuki10044 <iharayuki0103@gmail.com>
2020-03-20 17:09:48 +0900

    sukosi susumeta

--- include/local_map_creator/local_map_creator.h
+++ include/local_map_creator/local_map_creator.h
@@ -3,27 +3,33 @@
 
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
-#include "geometry_msgs/Pose2D"
 #include "nav_msgs/OccupancyGrid"
 
-class Grid_Map_Creator
+struct laser_data{
+    double angle;
+    double range;
+}
+
+class Local_Map_Creator
 {
  public:
-     Grid_Map_Creator();
+     Local_Map_Creator();
 
  private:
     //method
     void point_cloud_callback(const sensor_msgs::LaserScan::ConstPtr&);
-    void pose_callback(const geometry_msgs::Pose2D::ConstPtr&);
-    void get_global_map();
     void update_grid_map();//Local Mapの作成及び更新を行う
-    void compare_maps();
-    void update_global_map();
-
 
     //parameter
+    int gmap_width;
+    int gmap_height;
+    double gmap_resolution;
+    int obstacle_value;
+    int safe_value;
 
     //member
+    ros::Publisher pub_laser;
+    ros::Subscriber sub_grid;
     sensor_msgs::LaserScan
-
+    nav_msgs::OccupancyGrid
 };
